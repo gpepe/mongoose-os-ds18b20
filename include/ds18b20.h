38339a30c67d514101c1dd946cb87a44cefe0d2b
Górász Péter <pepe@ezkell.hu>
2017-12-13 20:54:32 +0100

    undefined reference fix

--- include/ds18b20.h
+++ include/ds18b20.h
@@ -1,3 +1,10 @@
+#ifndef CS_MOS_LIBS_DS18B20_H_
+#define CS_MOS_LIBS_DS18B20_H_
+
+#if defined(__cplusplus)
+extern "C" {  // Make sure we have C-declarations in C++ programs
+#endif
+
 struct ds18b20_result {
     uint8_t rom[8];
     char *mac;
@@ -8,3 +15,11 @@ struct ds18b20_result {
 typedef void (*ds18b20_read_t)(struct ds18b20_result *results);
 
 void ds18b20_read_all(int pin, int resolutionBits, ds18b20_read_t callback);
+
+
+#if defined(__cplusplus)
+}
+#endif
+
+
+#endif
